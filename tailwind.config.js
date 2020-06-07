b4b0963d9b1c72ee9292bc8fefaca53b57871072
Seymos76 <contact@louisthomas.fr>
2020-06-07 20:36:20 +0200

    building homepage with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    theme: {
+        screens: {
+            xs: { 'min': '320px', 'max': '639px' },
+            sm: { 'min': '640px', 'max': '767px' },
+            md: { 'min': '768px', 'max': '1023px' },
+            lg: { 'min': '1024px', 'max': '1279px' },
+            xl: { 'min': '1280px', 'max': '1920px' },
+        },
+        fontFamily: {
+            display: ['Gilroy', 'sans-serif'],
+            body: ['Graphik', 'sans-serif'],
+        },
+        borderWidth: {
+            default: '1px',
+            '0': '0',
+            '2': '2px',
+            '4': '4px',
+        },
+        borderColor: {
+            primary: '#e2faff',
+        },
+        extend: {
+            colors: {
+                cyan: '#9cdbff',
+            },
+            spacing: {
+                '96': '24rem',
+                '128': '32rem',
+            }
+        }
+    }
+}
